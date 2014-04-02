3d0078eb4ee003d2e90ebffc4a17c4e3bc2e465d
Yuelin37 <yuelinyan@gmail.com>
2014-04-02 14:59:32 +0800

    orm draft stable

--- exercise/timetrack/app.js
+++ exercise/timetrack/app.js
@@ -17,66 +17,33 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon());
+// app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 
-app.use(orm.express("mysql://root:root@localhost/timetrack", {
-    define: function (db, models, next) {
-        models.person = db.define("person", {
-        name: { type: 'text', required: true },
-        surname: { type: 'text', required: true },
-        age: { type: 'number', required: false }
-    }, {
-        methods: {
-            fullName: function() {
-                return this.name + ' ' + this.surname;
-            }
-        },
-        validations: {
-            age: orm.enforce.ranges.number(18, undefined, "under-age")
-        }
-    });
-        next();
-    }
-}));
+app.use(function (req, res, next) {
+      models(function (err, db) {
+        if (err) return next(err);
+
+        req.models = db.models;
+        req.db     = db;
+
+        return next();
+      });
+    }),
 
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// // development only
-// if ('development' == app.get('env')) {
-//   app.use(express.errorHandler());
-// }
 
 // app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/add', user.add);
 
 // http.createServer(app).listen(app.get('port'), function(){
 //   console.log('Express server listening on port ' + app.get('port'));
 // });
 
-
-
 app.listen(3000);
-
-// app.get('/users', function (req, res) {
-//     // req.models is a reference to models used above in define()
-//     req.models.person.find({
-//         surname: "Doe"
-//     }, function(err, people) {
-//         // SQL: "SELECT * FROM person WHERE surname = 'Doe'"
-
-//         console.log("People found: %d", people.length);
-//         console.log("First person: %s, age %d", people[0].fullName(), people[0].age);
-
-//         people[0].age = 16;
-//         people[0].save(function(err) {
-//             // err.msg = "under-age";
-//         });
-
-//         res.end(people[0].fullName() + ':' + people[0].age);
-//     });
-// });
--- exercise/timetrack/routes/user.js
+++ exercise/timetrack/routes/user.js
@@ -1,37 +1,37 @@
-
 /*
  * GET users listing.
  */
 
 // var orm = require('orm');
 
-exports.list = function(req, res){
-	// req.models.user.find().limit(4).order('-id').all(function(err, messages) {
- //      if (err) return next(err);
+exports.list = function(req, res) {
+	req.models.person.find({
+		surname: "Doe"
+	}, function(err, people) {
+		// SQL: "SELECT * FROM person WHERE surname = 'Doe'"
 
- //      var items = messages.map(function(m) {
- //        return m.serialize();
- //      });
+		console.log("People found: %d", people.length);
+		console.log("First person: %s, age %d", people[0].fullName(), people[0].age);
+		people[0].remove();
+		res.render('users', {
+			title: 'Photos',
+			users: people
+		});
+	});
+};
 
- //      res.send({
- //        items: items
- //      });
- //    });
-  // res.send("respond with a resource");
+exports.add = function(req, res) {
+	req.models.person.create({
+		name: "Yan",
+		surname: "Doe",
+		age: 100
+	}, function(err, people) {
+		if (err) throw err;
 
-  req.models.person.find({
-        surname: "Doe"
-    }, function(err, people) {
-        // SQL: "SELECT * FROM person WHERE surname = 'Doe'"
-
-        console.log("People found: %d", people.length);
-        console.log("First person: %s, age %d", people[0].fullName(), people[0].age);
-
-        people[0].age = 16;
-        people[0].save(function(err) {
-            // err.msg = "under-age";
-        });
-
-        res.end(people[0].fullName() + ':' + people[0].age + 'hehe...');
-    });
+		// db.close()
+		console.log("Done!");
+		people.age = 25;
+		people.save();
+		res.end('Done.');
+	});
 };
