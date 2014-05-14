dfc3343478ddebee46dc5f9d6f32001036c6981a
Yuelin37 <yuelinyan@gmail.com>
2014-05-14 12:35:13 +0800

    dojo5

---
+++ exercise/kata/public/js/todos.js
@@ -0,0 +1,8 @@
+function TodosController($scope){
+	$scope.todos = [{
+		title: 'Todo_1'
+	},{
+		title: 'Todo_2'
+	}
+	]
+}
---
+++ exercise/kata/server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var jsonBody = require("body/json");
+var mongoose = require('mongoose');
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/todos');
+var Todo = mongoose.model('todos', new mongoose.Schema({
+	title: String,
+	content: String
+}));
+
+app.get('/todos', function(req, res) {
+	Todo.find(function(err, data) {
+		res.send(data);
+	});
+});
+
+app.post('/todos', function(req, res) {
+	jsonBody(req, res, function(err, body) {
+		var todo = new Todo(body);
+
+		todo.save(function() {
+			console.log(body);
+			res.send(body);
+		});
+	});
+});
+
+app.listen(9432);
+console.log('localhost:9432 running...');
